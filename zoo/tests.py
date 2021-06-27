2eef9c9db1227c3d8ff09208d1b03d743dd0141a
chirsch <cecile.h.75@gmail.com>
2021-06-27 18:21:12 +0200

    zooland

--- zoo/tests.py
+++ zoo/tests.py
@@ -42,48 +42,48 @@ class calculAgeNoneTestCase(TestCase):
         self.assertEqual(age, "Unknown")
         
 
-# class MoveAnimalEnclosueTestCase(TestCase):
-#     def setUp(self):
-#         species_a = Species()
-#         species_a.name = "Dauphin"
-#         species_a.diet_type = Species.DietType.CARNIVORE
-#         species_a.save()
+class MoveAnimalEnclosueTestCase(TestCase):
+    def setUp(self):
+        species_a = Species()
+        species_a.name = "Dauphin"
+        species_a.diet_type = Species.DietType.CARNIVORE
+        species_a.save()
         
-#         enclosure_1 = Enclosure(name="Carnivores", capacity=5)
-#         enclosure_2 = Enclosure(name="Grand bassin", capacity=2)
-#         enclosure_1.save()
-#         enclosure_2.save()
+        enclosure_1 = Enclosure(name="Carnivores", capacity=5)
+        enclosure_2 = Enclosure(name="Grand bassin", capacity=2)
+        enclosure_1.save()
+        enclosure_2.save()
         
-#         animal_a = Animal(name="Fliper", species = species_a, enclosure=enclosure_1, date_of_birth=None, image='image')
-#         animal_a.save()
+        animal_a = Animal(name="Fliper", species = species_a, enclosure=enclosure_1, date_of_birth=None, image='image')
+        animal_a.save()
         
-#     def test_move_animal(self):
-#         fliper = Animal.objects.first()
-#         response = self.client.post(
-#             reverse('move_animal', 
-#                     kwargs = {'id': fliper.id}), 
-#                     {'enclosure': 2}
-#         )
-#         print(response)
-#         self.assertEqual(response.status_code, 302)
+    def test_move_animal(self):
+        fliper = Animal.objects.first()
+        response = self.client.post(
+            reverse('move_animal', 
+                    kwargs = {'id': fliper.id}), 
+                    {'enclosure': 2}
+        )
+        print(response)
+        self.assertEqual(response.status_code, 302)
 
-#         fliper.refresh_from_db()
-#         # self.assertEqual(fliper.enclosure, enclosure) 
+        fliper.refresh_from_db()
+        # self.assertEqual(fliper.enclosure, enclosure) 
 
-#     def test_can_send_message(self):
-#         fliper = Animal.objects.first()
-#         data = {
-#             "enclosure": 1,
-#         }
-#         response = self.client.get(reverse('animal', 
-#                            kwargs = {'id': fliper.id}))
-#         self.assertTemplateUsed(response, "zoo/show_animal.html")
-#         self.assertContains(response, "1")
-#         response = self.client.post(reverse('animal', 
-#                            kwargs = {'id': fliper.id}), data=data)
-#         self.assertEqual(Enclosure.objects.count(), 2)
-#         self.assertRedirects(response, reverse('animal', 
-#                                                kwargs = {'id': fliper.id}))
+    def test_can_send_message(self):
+        fliper = Animal.objects.first()
+        data = {
+            "enclosure": 1,
+        }
+        response = self.client.get(reverse('animal', 
+                           kwargs = {'id': fliper.id}))
+        self.assertTemplateUsed(response, "zoo/show_animal.html")
+        self.assertContains(response, "1")
+        response = self.client.post(reverse('animal', 
+                           kwargs = {'id': fliper.id}), data=data)
+        self.assertEqual(Enclosure.objects.count(), 2)
+        self.assertRedirects(response, reverse('animal', 
+                                               kwargs = {'id': fliper.id}))
         
         
 class PluralizeTest(TestCase):
